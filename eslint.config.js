5e7defaebafae800d1956d98c85e4025e47df89c
abdullah <muhammadabdullahiqbal2021@gmail.com>
2025-03-26 12:34:25 +0500

    Add basic eslint plugins and rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
 	{ ignores: ['dist'] },
@@ -16,6 +17,7 @@ export default tseslint.config(
 		plugins: {
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
+			import: importPlugin,
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
@@ -23,6 +25,24 @@ export default tseslint.config(
 				'warn',
 				{ allowConstantExport: true },
 			],
+			'import/order': [
+				'warn',
+				{
+					groups: ['builtin', 'external', 'internal'],
+					'newlines-between': 'always',
+				},
+			],
+			'import/no-duplicates': 'error',
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_', // Ignore function arguments starting with `_`
+					varsIgnorePattern: '^_', // Ignore all variables starting with `_`
+					caughtErrors: 'all', // Ignore all caught errors
+					destructuredArrayIgnorePattern: '^_', // Ignore destructured variables starting with `_`
+				},
+			],
+			'@typescript-eslint/no-explicit-any': 'warn',
 		},
 	},
 );
