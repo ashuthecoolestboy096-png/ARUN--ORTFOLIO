51c277550f44f27dd11473766e4350a14a213d77
abdullah <muhammadabdullahiqbal2021@gmail.com>
2025-03-27 11:12:48 +0500

    Add scrollbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { MouseTrail } from './components/mouse-trail';
+import { ScrollBar } from './components/scroll-bar';
 import { InfoSection } from './sections/info.section';
 import './styles/floating-button.css';
 import './styles/glow-box.css';
 import './styles/info-section.css';
+import './styles/mouse-trail.css';
 import './styles/text-hover.css';
 
 function App() {
-	return <InfoSection />;
+	return (
+		<>
+			<ScrollBar />
+			<MouseTrail />
+			<InfoSection />
+		</>
+	);
 }
 
 export default App;
